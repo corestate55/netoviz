477f1a6f9dac47b6000e8482a2d80fb70353f1a5
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2018-07-18 20:29:06 +0900

    add tooltip sample

--- src/single-visualizer.js
+++ src/single-visualizer.js
@@ -4,18 +4,25 @@ import * as d3 from 'd3'
 import {ForceSimulator} from './force-simulator'
 
 export class SingleGraphVisualizer {
-  constructor (graph) {
+  constructor (graph, findAllNodeFunc) {
     this.graph = graph
     this.setCavasSize()
+    this.visContainer = d3.select('body')
+      .select('div#visualizer')
+      .append('div')
+      .attr('class', 'networklayer')
+    console.log(findAllNodeFunc)
+    this.findGraphNodeByPath = findAllNodeFunc
   }
 
-  setHighlightNodeCallback (func) {
-    this.highlightNode = func
+  makeToolTip () {
+    return this.visContainer
+      .append('div')
+      .attr('class', 'tooltip')
   }
 
   makeNetworkLayer () {
-    return d3.select('body')
-      .select('div#visualizer')
+    return this.visContainer
       .append('svg')
       .attr('width', this.width)
       .attr('height', this.height)
@@ -101,6 +108,7 @@ export class SingleGraphVisualizer {
     this.renameLinkKey()
 
     this.nwLayer = this.makeNetworkLayer()
+    this.tooltip = this.makeToolTip()
     this.clearBtn = this.makeClearButton()
     this.link = this.makeLinkObjects()
     this.tp = this.makeTpObjects()
@@ -124,45 +132,114 @@ export class SingleGraphVisualizer {
 
   setEventCallBack (objs) {
     var self = this // alias to use event callback closure
+
+    function clearElementHighlight (element) {
+      ['selectedchildren', 'selectedparents', 'selected'].forEach(
+        d => element.classList.remove(d)
+      )
+    }
+
+    // highlight selected node
+    function highlightNodeByPath (direction, path) {
+      var element = document.getElementById(path)
+      clearElementHighlight(element)
+      if (direction === 'children') {
+        element.classList.add('selectedchildren')
+      } else if (direction === 'parents') {
+        element.classList.add('selectedparents')
+      } else {
+        element.classList.add('selected')
+      }
+    }
+
+    // event callback
+    function highlightNode (element) {
+      function findSupportingObj (direction, path) {
+        // highlight DOM
+        console.log('....', direction, path)
+        highlightNodeByPath(direction, path)
+        // find nodes to highlight via through *all* layers
+        var node = self.findGraphNodeByPath(path)
+        if (node[direction]) {
+          // search children/parent recursively
+          node[direction].forEach(
+            d => findSupportingObj(direction, d)
+          )
+        }
+      }
+      // highlight selected object and its children/parents
+      var path = element.getAttribute('id')
+      console.log('highlight_top: ', path)
+      findSupportingObj('children', path)
+      findSupportingObj('parents', path)
+      findSupportingObj('clicked', path) // dummy direction
+    }
+
+    function mouseOver (element) {
+      element.classList.add('selectready')
+      var node = self.findGraphNodeByPath(element.id)
+      if (node) {
+        console.log('mouseover, ', node)
+        self.tooltip
+          .style('visibility', 'visible')
+          .html('element: ' + node.path)
+      } else {
+        self.tooltip
+          .style('visibility', 'visible')
+          .html('element: ' + element.id)
+      }
+    }
+
+    function mouseMove (element) {
+      self.tooltip
+        .style('top', (d3.event.pageY - 20) + 'px')
+        .style('left', (d3.event.pageX + 10) + 'px')
+    }
+
+    function mouseOut (element) {
+      element.classList.remove('selectready')
+      self.tooltip
+        .style('visibility', 'hidden')
+        .html('')
+    }
+
+    // set event callbacks
     objs.forEach(obj => {
       // use `function() {}` NOT arrow-function `() => {}`.
       // arrow-function bind `this` according to decrared position
       obj
-        .on('click', function () { self.highlightNode(this) })
-        .on('mouseover', function () { self.mouseOver(this) })
-        .on('mouseout', function () { self.mouseOut(this) })
+        .on('click', function () { highlightNode(this) })
+        .on('mouseover', function () { mouseOver(this) })
+        .on('mousemove', function () { mouseMove(this) })
+        .on('mouseout', function () { mouseOut(this) })
         .call(d3.drag()
-          .on('start', self.simulator.dragstarted)
-          .on('drag', self.simulator.dragged)
-          .on('end', self.simulator.dragended))
+              .on('start', self.simulator.dragstarted)
+              .on('drag', self.simulator.dragged)
+              .on('end', self.simulator.dragended))
     })
-  }
-
-  clearHighlight () {
-    // clear all highlighted object
-    var element = document.getElementById('visualizer');
-    ['selectedchildren', 'selectedparents', 'selected'].forEach(d => {
-      var selectedElements = element.getElementsByClassName(d)
-      Array.from(selectedElements).forEach(element => {
-        element.classList.remove(d)
-      })
-    })
-  }
-
-  mouseOver (element) {
-    element.classList.add('selectready')
-  }
-
-  mouseOut (element) {
-    element.classList.remove('selectready')
   }
 
   setClearButtonEventCallback () {
-    var self = this // alias to use event callback closure
+    function clearHighlight () {
+      // clear all highlighted object
+      var element = document.getElementById('visualizer');
+      ['selectedchildren', 'selectedparents', 'selected'].forEach(d => {
+        var selectedElements = element.getElementsByClassName(d)
+        Array.from(selectedElements).forEach(element => {
+          element.classList.remove(d)
+        })
+      })
+    }
+
     // set event callback for clear button
+    // NOTICE: `this`
     this.clearBtn
-      .on('click', self.clearHighlight)
-      .on('mouseover', function () { self.mouseOver(this) }) // NOTICE: `this`
-      .on('mouseout', function () { self.mouseOut(this) })
+      .on('click', clearHighlight)
+      .on('mouseover', function () {
+        this.classList.add('selectready')
+      })
+      .on('mouseout', function () {
+        this.classList.remove('selectready')
+      })
   }
 }
--- src/visualizer.js
+++ src/visualizer.js
@@ -9,57 +9,12 @@ export class GraphVisualizer extends Graphs {
   }
 
   drawGraphs () {
-    var self = this // alias to use event callback closure
-
-    function clearElementHighlight (element) {
-      ['selectedchildren', 'selectedparents', 'selected'].forEach(
-        d => element.classList.remove(d)
-      )
-    }
-
-    // highlight selected node
-    function highlightNodeByPath (direction, path) {
-      var element = document.getElementById(path)
-      clearElementHighlight(element)
-      if (direction === 'children') {
-        element.classList.add('selectedchildren')
-      } else if (direction === 'parents') {
-        element.classList.add('selectedparents')
-      } else {
-        element.classList.add('selected')
-      }
-    }
-
-    // event callback
-    function highlightNode (element) {
-      function findSupportingObj (direction, path) {
-        // highlight DOM
-        console.log('....', direction, path)
-        highlightNodeByPath(direction, path)
-        // find nodes to highlight via through *all* layers
-        var node = self.findGraphNodeByPath(path)
-        if (node[direction]) {
-          // search children/parent recursively
-          node[direction].forEach(
-            d => findSupportingObj(direction, d)
-          )
-        } else {
-          console.log('attribute: ', node.attribute)
-        }
-      }
-      // highlight selected object and its children/parents
-      var path = element.getAttribute('id')
-      console.log('highlight_top: ', path)
-      findSupportingObj('children', path)
-      findSupportingObj('parents', path)
-      findSupportingObj('clicked', path) // dummy direction
-    }
-
+    // hand-over the operation through all layers
+    // NOTICE: BIND `this`
+    var callback = path => this.findGraphNodeByPath(path)
     // entrypoint: draw each layer
     this.graphs.forEach(graph => {
-      var sgv = new SingleGraphVisualizer(graph)
-      // handout operation through all layers
-      sgv.setHighlightNodeCallback(highlightNode)
+      var sgv = new SingleGraphVisualizer(graph, callback)
       sgv.draw()
     })
   }
