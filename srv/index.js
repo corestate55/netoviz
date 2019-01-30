c48c38e672869c432d99f35e85bcbe9216a590d8
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-01-30 13:14:44 +0900

    refactoring srver

--- srv/index.js
+++ srv/index.js
@@ -1,56 +1,18 @@
-import fs from 'fs'
 import express from 'express'
-import Graphs from './graph/graphs'
-import DepGraphConverter from './data-converter'
+import TopologyDataAPI from './topology-data-api'
 
 const port = process.env.PORT || 3000 // process.env.PORT for Heroku
-const timeStampOf = {}
-const prodDistDir = 'dist'
-const devDistDir = 'public'
-const distDir = process.env.NODE_ENV === 'production' ? prodDistDir : devDistDir
-const modelDir = `${distDir}/model`
-
-function convertTopoGraphData (req) {
-  const jsonName = req.params.jsonName
-  const jsonPath = `${modelDir}/${jsonName}`
-  const cacheJsonPath = `${prodDistDir}/${jsonName}.cache` // always use prod dist dir
-  console.log('Requested: ', jsonPath)
-
-  const timeStamp = fs.statSync(jsonPath)
-  let resJsonString = '' // stringified json (NOT object)
-  if (timeStampOf[jsonPath] && timeStampOf[jsonPath] === timeStamp.mtimeMs) {
-    // the json file has not been modified.
-    console.log('use cache: ', cacheJsonPath)
-    resJsonString = fs.readFileSync(cacheJsonPath, 'utf8')
-  } else {
-    // the json file was changed.
-    timeStampOf[jsonPath] = timeStamp.mtimeMs
-    // read data from the json file
-    const topoData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'))
-    const graphs = new Graphs(topoData)
-    console.log('create cache: ', cacheJsonPath)
-    resJsonString = JSON.stringify(graphs.graphs)
-    // save cache
-    fs.writeFileSync(cacheJsonPath, resJsonString)
-  }
-  return resJsonString
-}
-
-function convertDependencyGraphData (req) {
-  const topoJsonString = convertTopoGraphData(req)
-  const depGraph = new DepGraphConverter(JSON.parse(topoJsonString))
-  return JSON.stringify(depGraph.toData())
-}
+const topoDataAPI = new TopologyDataAPI(process.env.NODE_ENV)
 
 export default app => {
   app.use(express.json())
   app.set('port', port)
   app.get('/draw/:jsonName', function (req, res) {
     res.type('json')
-    res.send(convertTopoGraphData(req))
+    res.send(topoDataAPI.convertTopoGraphData(req))
   })
   app.get('/draw-dep-graph/:jsonName', function (req, res) {
     res.type('json')
-    res.send(convertDependencyGraphData(req))
+    res.send(topoDataAPI.convertDependencyGraphData(req))
   })
 }
---
+++ srv/topology-data-api.js
@@ -0,0 +1,75 @@
+import fs from 'fs'
+import Graphs from './graph/graphs'
+import DepGraphConverter from './dep-graph-converter'
+
+export default class TopoogyDataAPI {
+  constructor (mode) {
+    this.timeStampOf = {} // timestamp table
+    const prodDistDir = 'dist'
+    const devDistDir = 'public'
+    const distDir = mode === 'production' ? prodDistDir : devDistDir
+    this.modelDir = `${distDir}/model`
+    this.cacheDir = prodDistDir // always use prod dist dir for cache
+    this.checkCacheDir()
+  }
+
+  checkCacheDir () {
+    if (!fs.existsSync(this.cacheDir)) {
+      fs.mkdirSync(this.cacheDir)
+    }
+  }
+
+  readCache () {
+    console.log('use cache: ', this.cacheJsonPath)
+    return fs.readFileSync(this.cacheJsonPath, 'utf8')
+  }
+
+  readTopologyDataFromJSON () {
+    const topoData = JSON.parse(fs.readFileSync(this.jsonPath, 'utf8'))
+    const graphs = new Graphs(topoData)
+    return JSON.stringify(graphs.graphs)
+  }
+
+  writeCache (resJsonString) {
+    console.log('create cache: ', this.cacheJsonPath)
+    fs.writeFileSync(this.cacheJsonPath, resJsonString)
+  }
+
+  updateStatsOfTopoJSON (req) {
+    this.jsonName = req.params.jsonName
+    this.jsonPath = `${this.modelDir}/${this.jsonName}`
+    this.cacheJsonPath = `${this.cacheDir}/${this.jsonName}.cache`
+    this.timeStamp = fs.statSync(this.jsonPath)
+  }
+
+  foundCache () {
+    return this.timeStampOf[this.jsonPath] &&
+      this.timeStampOf[this.jsonPath] === this.timeStamp.mtimeMs
+  }
+
+  updateCacheTimeStamp () {
+    this.timeStampOf[this.jsonPath] = this.timeStamp.mtimeMs
+  }
+
+  convertTopoGraphData (req) {
+    this.updateStatsOfTopoJSON(req)
+    console.log('Requested: ', this.jsonPath)
+
+    let resJsonString = '' // stringified json (NOT object)
+    if (this.foundCache()) {
+      resJsonString = this.readCache()
+    } else {
+      // the json file was changed.
+      this.updateCacheTimeStamp()
+      resJsonString = this.readTopologyDataFromJSON()
+      this.writeCache(resJsonString)
+    }
+    return resJsonString
+  }
+
+  convertDependencyGraphData (req) {
+    const topoJsonString = this.convertTopoGraphData(req)
+    const depGraph = new DepGraphConverter(JSON.parse(topoJsonString))
+    return JSON.stringify(depGraph.toData())
+  }
+}
