e10542e7071f8a9e9a0a5d471bb777ceb134e566
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-03-18 19:25:58 +0900

    fix dbmigrate script

--- bin/dbmigrate.sh
+++ bin/dbmigrate.sh
@@ -5,8 +5,14 @@ SEQUELIZE="node_modules/.bin/sequelize"
 SRV_DIR="srv"
 CONFIG="${SRV_DIR}/config/config.json"
 
+MODE="development"
+if test ! -z ${NODE_ENV}; then
+  MODE=${NODE_ENV}
+fi
+
+# delete SRV_DIR (server directory) from config when migration
 sed -i -e "s/${SRV_DIR}\///g" ${CONFIG}
 cd ${SRV_DIR}
-${CWD}/${SEQUELIZE} db:migrate --env development
+${CWD}/${SEQUELIZE} db:migrate --env ${MODE}
 cd ${CWD}
-git checkout ${CONFIG}
+sed -i -e "s/\([a-zA-Z0-9]*.sqlite3\)/${SRV_DIR}\/\1/g" ${CONFIG}
