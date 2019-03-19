572b4393c6df2d59a06379c45d67967a6c7763d2
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-03-19 23:04:22 +0900

    add handle and drag event on grid line

--- src/graph/nested/operational-visualizer.js
+++ src/graph/nested/operational-visualizer.js
@@ -1,12 +1,54 @@
-import { zoom } from 'd3-zoom'
 import { event } from 'd3-selection'
+import { zoom } from 'd3-zoom'
+import { drag } from 'd3-drag'
 import SingleNestedGraphVisualizer from './single-visualizer'
 
 export default class OperationalNestedGraphVisualizer extends SingleNestedGraphVisualizer {
-  setOperationHandler (graphData) {
+  setXGridHandler () {
+    const dragged = (d, i) => {
+      d.position = event.x
+      this.svgGrp.select(`line#grid-x${i}`)
+        .attr('x1', d.position)
+        .attr('x2', d.position)
+      this.svgGrp.select(`circle#grid-x${i}-handle`)
+        .attr('cx', d.position)
+    }
+    this.svgGrp.selectAll('circle.grid-x-handle')
+      .call(drag()
+        // .on('start', dragStarted)
+        .on('drag', dragged)
+        // .on('end', dragEnded)
+      )
+  }
+
+  setYGridHandler () {
+    const dragged = (d, i) => {
+      d.position = event.y
+      this.svgGrp.select(`line#grid-y${i}`)
+        .attr('y1', d.position)
+        .attr('y2', d.position)
+      this.svgGrp.select(`circle#grid-y${i}-handle`)
+        .attr('cy', d.position)
+    }
+    this.svgGrp.selectAll('circle.grid-y-handle')
+      .call(drag()
+        // .on('start', dragStarted)
+        .on('drag', dragged)
+        // .on('end', dragEnded)
+      )
+  }
+
+  setSVGZoom () {
     this.svg.call(zoom()
       .scaleExtent([1 / 4, 5])
+      .translateExtent([[-150, -150], [2000, 2000]])
       .on('zoom', () => this.svgGrp.attr('transform', event.transform))
     )
+  }
+
+  setOperationHandler (graphData) {
+    this.setXGridHandler()
+    this.setYGridHandler()
+    this.setSVGZoom()
   }
 }
--- src/graph/nested/single-visualizer.js
+++ src/graph/nested/single-visualizer.js
@@ -9,6 +9,9 @@ export default class SingleNestedVisualizer extends BaseContainer {
     this.width = 800
     this.height = 600
     this.fontSize = 10
+    this.gridStart = -100
+    this.gridEnd = 2000
+    this.gridHandleRadius = 25
   }
 
   clearCanvas () {
@@ -32,38 +35,62 @@ export default class SingleNestedVisualizer extends BaseContainer {
       .attr('id', 'whole-dep-graph')
   }
 
-  makeGraphObjects (graphData) {
-    this.svg = this.makeNestedGraphSVG()
-    this.svgGrp = this.makeNestedGraphSVGGroup()
-
-    const nodes = graphData.nodes.filter(d => d.type === 'node')
-    const tps = graphData.nodes.filter(d => d.type === 'tp')
-
+  makeXGrids (xGridData) {
+    const xGrids = xGridData.map((d, i) => {
+      return { position: d, index: i }
+    })
     this.svgGrp.selectAll('line.grid-x')
-      .data(graphData.grid.x)
+      .data(xGrids)
       .enter()
       .append('line')
       .attr('class', 'grid-x')
-      .attr('x1', d => d)
-      .attr('y1', -100)
-      .attr('x2', d => d)
-      .attr('y2', 2000)
+      .attr('id', d => `grid-x${d.index}`)
+      .attr('x1', d => d.position)
+      .attr('y1', this.gridStart)
+      .attr('x2', d => d.position)
+      .attr('y2', this.gridEnd)
+    this.svgGrp.selectAll('circle.grid-x-handle')
+      .data(xGrids)
+      .enter()
+      .append('circle')
+      .attr('class', 'grid-x-handle')
+      .attr('id', d => `grid-x${d.index}-handle`)
+      .attr('cx', d => d.position)
+      .attr('cy', this.gridStart)
+      .attr('r', this.gridHandleRadius)
+  }
 
+  makeYGrids (yGridData) {
+    const yGrids = yGridData.map((d, i) => {
+      return { position: d, index: i }
+    })
     this.svgGrp.selectAll('line.grid-y')
-      .data(graphData.grid.y)
+      .data(yGrids)
       .enter()
       .append('line')
       .attr('class', 'grid-y')
-      .attr('x1', -100)
-      .attr('y1', d => d)
-      .attr('x2', 2000)
-      .attr('y2', d => d)
+      .attr('id', (d) => `grid-y${d.index}`)
+      .attr('x1', this.gridStart)
+      .attr('y1', d => d.position)
+      .attr('x2', this.gridEnd)
+      .attr('y2', d => d.position)
+    this.svgGrp.selectAll('circle.grid-y-handle')
+      .data(yGrids)
+      .enter()
+      .append('circle')
+      .attr('class', 'grid-y-handle')
+      .attr('id', d => `grid-y${d.index}-handle`)
+      .attr('cx', this.gridStart)
+      .attr('cy', d => d.position)
+      .attr('r', this.gridHandleRadius)
+  }
 
-    // node
-    this.svgGrp.selectAll('rect')
+  makeNodes (nodes) {
+    this.svgGrp.selectAll('rect.node')
       .data(nodes)
       .enter()
       .append('rect')
+      .attr('class', 'node')
       .attr('id', d => d.path)
       .attr('x', d => d.x)
       .attr('y', d => d.y)
@@ -73,11 +100,11 @@ export default class SingleNestedVisualizer extends BaseContainer {
       .attr('ry', 5)
       .append('title')
       .text(d => d.path)
+  }
 
-    // lines
-    const linkCreator = new InterTpLinkCreator(graphData)
+  makeSupportTpLines (supportTpLinks) {
     this.svgGrp.selectAll('line.support-tp')
-      .data(linkCreator.supportTpLinks())
+      .data(supportTpLinks)
       .enter()
       .append('line')
       .attr('class', d => d.type)
@@ -85,28 +112,33 @@ export default class SingleNestedVisualizer extends BaseContainer {
       .attr('y1', d => d.y1)
       .attr('x2', d => d.x2)
       .attr('y2', d => d.y2)
+  }
 
+  makeTpTpLines (tpTpLinks) {
     this.svgGrp.selectAll('polyline.tp-tp')
-      .data(linkCreator.tpTpLinks())
+      .data(tpTpLinks)
       .enter()
       .append('polyline')
       .attr('class', d => d.type)
       .attr('id', d => d.path)
       .attr('points', d => d.polylineString())
+  }
 
-    // tp
-    this.svgGrp.selectAll('circle')
+  makeTp (tps) {
+    this.svgGrp.selectAll('circle.tp')
       .data(tps)
       .enter()
       .append('circle')
+      .attr('class', 'tp')
       .attr('id', d => d.path)
       .attr('cx', d => d.cx)
       .attr('cy', d => d.cy)
       .attr('r', d => d.r)
       .append('title')
       .text(d => d.path)
+  }
 
-    // node label
+  makeNodeLabels (nodes) {
     this.svgGrp.selectAll('text.node')
       .data(nodes)
       .enter()
@@ -116,8 +148,9 @@ export default class SingleNestedVisualizer extends BaseContainer {
       .attr('y', d => d.y + d.height)
       .attr('alignment-baseline', 'hanging')
       .text(d => d.name)
+  }
 
-    // tp label
+  makeTpLabels (tps) {
     this.svgGrp.selectAll('text.tp')
       .data(tps)
       .enter()
@@ -128,5 +161,26 @@ export default class SingleNestedVisualizer extends BaseContainer {
       .attr('text-anchor', 'middle')
       .attr('alignment-baseline', 'hanging')
       .text(d => d.name)
+  }
+
+  makeGraphObjects (graphData) {
+    this.svg = this.makeNestedGraphSVG()
+    this.svgGrp = this.makeNestedGraphSVGGroup()
+
+    this.makeXGrids(graphData.grid.x)
+    this.makeYGrids(graphData.grid.y)
+
+    const nodes = graphData.nodes.filter(d => d.type === 'node')
+    this.makeNodes(nodes)
+
+    const linkCreator = new InterTpLinkCreator(graphData)
+    this.makeSupportTpLines(linkCreator.supportTpLinks())
+    this.makeTpTpLines(linkCreator.tpTpLinks())
+
+    const tps = graphData.nodes.filter(d => d.type === 'tp')
+    this.makeTp(tps)
+
+    this.makeNodeLabels(nodes)
+    this.makeTpLabels(tps)
   }
 }
