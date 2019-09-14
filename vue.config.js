8df846821c1d1668c79bb0a11351491222c27dff
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-09-14 12:15:28 +0900

    Add stats-webpack-plugin and update packages.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
+const StatsPlugin = require('stats-webpack-plugin')
 module.exports = {
   pluginOptions: {
     express: {
       shouldServeApp: true,
       serverDir: './srv'
     }
+  },
+  configureWebpack: {
+    plugins: [new StatsPlugin('stats.json')]
   }
 }
