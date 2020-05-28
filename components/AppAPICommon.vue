f5b19edcf18d1c564b4de91410e452e48206f863
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2020-05-28 15:34:37 +0900

    Fix api url

--- components/AppAPICommon.vue
+++ components/AppAPICommon.vue
@@ -13,17 +13,19 @@ export default {
       }
     },
     grpcURIBase() {
+      const protocol = window.location.protocol
       const host = window.location.hostname
       const port = this.$env.NETOVIZ_GRPC_WEB_PORT
-      return `http://${host}:${port}`
+      return `${protocol}//${host}:${port}`
     },
     restURIBase() {
+      const protocol = window.location.protocol
       const host = window.location.hostname
       // If NETOVIZ_REST_PORT is defined,
       // use it instead of browser location port.
       // Run all-in-one application, unset NETOVIZ_REST_PORT.
       const port = this.$env.NETOVIZ_REST_PORT || window.location.port
-      return `http://${host}:${port}`
+      return `${protocol}//${host}:${port}`
     }
   }
 }
